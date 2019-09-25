b32cd16fa7ea8d222eff6ee399ebf25b19674d91
dudq <dudq.xmt@gmail.com>
2019-09-25 20:57:05 +0700

    update caculator.js file

--- caculator/caculator.js
+++ caculator/caculator.js
@@ -6,55 +6,47 @@ function get_num2() {
     let _num = parseInt(document.getElementById("idNum2").value);
     return _num;
 }
-
-/*
-var result = document.getElementById("idResult");
-function display(operator) {
-    result.innerHTML = get_num1() + operator + get_num2();
-}
-
-function caculator() {
-    let num1 = get_num1();
-    let num2 = get_num2();
-    document.getElementById("idAddition").addEventListener("click",display("+"));
-    //let document.getElementById("idSubtraction");
-    //let _mutiplication = document.getElementById("idMutiplication");
-    //let _division = document.getElementById("idDivision");
-    result.innerHTML;
+// phan mo rong
+function  caculator(e) {
+    let id = e.id;
+    let n1 = get_num1();
+    let n2 = get_num2();
+    let result = document.getElementById("idResult");
+    switch (id) {
+        case 'idAddition':
+            result.innerHTML = n1 + n2;
+            break;
+        case 'idSubtraction':
+            result.innerHTML = n1 - n2;
+            break;
+        case 'idMutiplication':
+            result.innerHTML = n1 * n2;
+            break;
+        case 'idDivision':
+            result.innerHTML = n1 / n2;
+            break;
     }
-*/
-
-
+}
+//4 ham cho 4 button
 function addition() {
-    //let _num1 = parseInt(document.getElementById("idNum1").value);
-    //let _num2 =parseInt(document.getElementById("idNum2").value);
     let _num1 = get_num1();
     let _num2 = get_num2();
     let _result = _num1 + _num2;
     document.getElementById("idResult").innerHTML ="Result:" + _result;
 }
 function subtraction() {
- /* let _num1 = parseInt(document.getElementById("idNum1").value);
-    let _num2 =parseInt(document.getElementById("idNum2").value);
- */
     let _num1 = get_num1();
     let _num2 = get_num2();
     let _result = _num1 - _num2;
     document.getElementById("idResult").innerHTML ="Result:" + _result;
 }
 function mutiplication() {
-    /*let _num1 = parseInt(document.getElementById("idNum1").value);
-    let _num2 =parseInt(document.getElementById("idNum2").value);
-    */
     let _num1 = get_num1();
     let _num2 = get_num2();
     let _result = _num1 * _num2;
     document.getElementById("idResult").innerHTML ="Result:" + _result;
 }
 function divition() {
-    /*let _num1 = parseInt(document.getElementById("idNum1").value);
-    let _num2 =parseInt(document.getElementById("idNum2").value);
-    */
     let _num1 = get_num1();
     let _num2 = get_num2();
     let _result = _num1 / _num2;
