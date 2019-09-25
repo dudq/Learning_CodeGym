294db9ecf79cf16b5d169ea8946381d6e3e36557
dudq <dudq.xmt@gmail.com>
2019-09-25 21:58:24 +0700

    add log_in file

---
+++ log_in/log_in.js
@@ -0,0 +1,11 @@
+let account = prompt(" Enter account:");
+if (account == "Admin") {
+    let password = prompt("Enter Password:");
+    if (password == "TheMaster") {
+        alert("Welcome!");
+    } else if (password == '') {
+        alert("Canceled");
+    } else alert("Wrong Password");
+} else if (account == '') {
+    alert("Canceled")
+} else alert("I don't know you")
