0e7e97b68e77c912ebd8a436ea828d105f124741
dudq <dudq.xmt@gmail.com>
2019-09-26 14:17:44 +0700

    add even_click folder

---
+++ even_click/even_click.js
@@ -0,0 +1,15 @@
+let imgObj = null;
+
+function init() {
+    imgObj = document.getElementById("ball");
+    imgObj.style.position = 'relative';
+    imgObj.style.left = '0px';
+    imgObj.style.height = '100px';
+    imgObj.style.width = '100px';
+
+}
+function moveRight() {
+    imgObj.style.left = parseInt(imgObj.style.left) + 10 + 'px';
+
+}
+
