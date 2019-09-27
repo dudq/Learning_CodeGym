e2870d910197d44734e4f3939a5472728982c421
dudq <dudq.xmt@gmail.com>
2019-09-27 15:02:29 +0700

    add game_do_you_lop_me file

---
+++ game_do_you_love_me/game_DYLM.js
@@ -0,0 +1,13 @@
+function yes() {
+    document.getElementById('yes')
+    alert('Yes.You are right');
+}
+
+function no() {
+    let left = Math.random() * window.innerWidth;
+    let top = Math.random() * window.innerHeight;
+    let no = document.getElementById('no');
+    no.style.left = left + 'px';
+    no.style.top = top + 'px';
+
+}
