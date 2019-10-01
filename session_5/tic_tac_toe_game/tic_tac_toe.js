8509ba61c9425b4a5a9b3bfde25ff07a9079978a
dudq <dudq.xmt@gmail.com>
2019-10-01 16:20:27 +0700

    add faking_textarea.html file

---
+++ session_5/tic_tac_toe_game/tic_tac_toe.js
@@ -0,0 +1,10 @@
+let data='';
+let tictactoe = document.getElementById('tic_tac_toe');
+let board = [];
+for (let i=0;i<3;i++){
+    board = ['.','.','.'];
+}
+for (let i= 0;i<3;i++){
+    data = '<br>';
+
+}
